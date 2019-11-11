4571b8b7c4f9980b47c776c8b9efeee365d88401
ngodi <albertngodi@gmail.com>
2019-11-11 15:17:05 +0100

    Start with create-react-app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="main-container">
-      <header className="header">
+    <div className='main-container'>
+      <header className='header'>
        <h1>React Calculator</h1>
       </header>
     </div>
   );
 }
-
 export default App;
