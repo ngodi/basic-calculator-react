ea777c7760d04f0b49fcb9b99494781d3666234e
ngodi <albertngodi@gmail.com>
2019-11-12 13:14:00 +0100

    calculate import into App.js

--- src/login/calcaulate.js
+++
@@ -1,9 +0,0 @@
-function calculate ({ total, next, operation }, buttonName){
-
-    const OPERATIONS = ['+','-','/','x','%','+/-']
-    if(OPERATIONS.includes(buttonName)){
-      total = operate(total,next,buttonName)
-    }
-    return total
-  }
-  export default calculate;
