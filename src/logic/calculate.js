9c47991154b36f5076647db8d55ccc62b04425a1
ngodi <albertngodi@gmail.com>
2019-11-12 09:04:08 +0100

    oprate method

---
+++ src/logic/calculate.js
@@ -0,0 +1,7 @@
+import operate from './operate';
+
+const calculate = (obj, btn) => {
+
+};
+export default calculate;
+//https://github.com/ahfarmer/calculator/blob/master/src/logic/calculate.js
---
+++ src/logic/operate.js
@@ -0,0 +1,21 @@
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  switch (operation) {
+    case '-':
+      return (Big(numberOne).minus(numberTwo).toString());
+    case 'X':
+      return (Big(numberOne).times(numberTwo).toString());
+    case '/':
+      if (Big(numberTwo) !== 0) {
+        return (Big(numberOne).div(numberTwo).toString());
+      }
+      break;
+    case '+':
+      return (Big(numberOne).plus(numberTwo).toString());
+    default:
+      return 'error';
+  }
+};
+
+export default operate;
