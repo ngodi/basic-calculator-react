16f56918c6214118509eb1d80bbe84c5bea42a42
ngodi <albertngodi@gmail.com>
2019-11-12 13:04:11 +0100

    calculate module

---
+++ src/login/calcaulate.js
@@ -0,0 +1,9 @@
+function calculate ({ total, next, operation }, buttonName){
+
+    const OPERATIONS = ['+','-','/','x','%','+/-']
+    if(OPERATIONS.includes(buttonName)){
+      total = operate(total,next,buttonName)
+    }
+    return total
+  }
+  export default calculate;
