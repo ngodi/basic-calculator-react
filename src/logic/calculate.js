64a61059c5a0ad31f66b2b6cb86f090a447c63fb
ngodi <albertngodi@gmail.com>
2019-11-14 05:32:16 +0100

    digits handler conditions in calculate method

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -2,8 +2,15 @@ import operate from './operate';
 
 // eslint-disable-next-line no-unused-vars
 const calculate = ({ total, next, operation }, buttonName) => {
-  const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
     if(DIGITS.includes(buttonName) ){
+      if(next.split('').includes('.') && buttonName == '.'){
+        return{
+          total,
+          next,
+          operation
+        }
+      }
       next += buttonName;
       return{
         total,
