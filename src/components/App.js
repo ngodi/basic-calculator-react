d605de5d998d97dcb16fa43c4142bca8f779fba9
ngodi <albertngodi@gmail.com>
2019-11-20 23:53:46 +0100

    set babel-eslint as parser

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -48,4 +48,4 @@ export default class ButtonPanel extends React.Component {
   </div>
     );
 }
-};
+}
